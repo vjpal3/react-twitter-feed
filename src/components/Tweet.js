c6019183441a05be06b8f2a29ca0da86ca6e1fdd
vjpal3 <vrishalipal@yahoo.com>
2018-09-28 11:46:17 -0400

    add date and event-handlers

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Tweet = (props) => {
+  let text = props.text.slice(0, props.text.length-props.displayUrl.length);
+
   return (
     <div className="grid-container tweet-div">
       <div className="grid-x grid-margin-x tweet-info-grid">
@@ -11,14 +13,17 @@ const Tweet = (props) => {
           />
         </div>
         <div className="cell small-10 info-cell">
-          <p className="tweet-info"><span className="user-info">
-                {props.userName}&nbsp;
-                <span className="user-info-date">
-                  @{props.userScreenName}
-                </span>
+          <p className="tweet-info">
+            <span className="user-info">
+              {props.userName}&nbsp;
+              <span className="user-info-date">
+                @{props.userScreenName} &ensp;
+                {props.dateStringProp}
               </span>
-          <br />
-          {props.text}</p>
+            </span>
+            <br />
+            {text}
+          </p>
 
           { props.mediaUrl.length > 0 &&
             <img className="mediaImage"
@@ -28,16 +33,20 @@ const Tweet = (props) => {
           }
 
           <p className="tweet-info">
-            <span className="icons reply"><i className="fa fa-reply" aria-hidden="true"></i></span> &emsp;&emsp;&emsp;
+            <span className="icons">
+              <i className="fa fa-reply" aria-hidden="true" onClick={() => {props.iconAlertProp('Reply')}}></i>
+            </span> &emsp;&emsp;&emsp;
             <span className={props.retweeted ? 'retweet' : 'icons'} >
-              <i className="fa fa-retweet" aria-hidden="true"></i>
+              <i className="fa fa-retweet" aria-hidden="true" onClick={() => {props.iconAlertProp('Retweet')}}></i>
               &nbsp;&nbsp; {props.retweetCount}
             </span> &emsp;&emsp;&emsp;
             <span className={props.favorited ? 'favorite' : 'icons'}>
-              <i className="fa fa-heart" aria-hidden="true"></i>
+              <i className="fa fa-heart" aria-hidden="true" onClick={() => {props.iconAlertProp('Like')}}></i>
               &nbsp;&nbsp; {props.favoriteCount}
             </span> &emsp;&emsp;&emsp;
-            <span className="icons more"><i className="fa fa-ellipsis-h" aria-hidden="true"></i></span>
+            <span className="icons">
+              <i className="fa fa-ellipsis-h" aria-hidden="true" onClick={() => {props.iconAlertProp('More...')}}></i>
+            </span>
           </p>
         </div>
       </div>
--- src/containers/TwitterFeed.js
+++ src/containers/TwitterFeed.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Tweet from '../components/Tweet';
+let karmaAlert = (event) => alert("Your Karma levelled up: 1");
 
 const TwitterFeed = (props) => {
+  let iconAlert = (iconName) => alert(iconName);
+
   let feed = props.data.map(tweetData => {
     let media_url="";
+    let display_url="";
     if(Object.getOwnPropertyNames(tweetData.entities).length !== 0) {
       media_url=tweetData.entities.media[0].media_url
+      display_url=tweetData.entities.media[0].display_url
     }
+
+    let date = new Date(parseInt(tweetData.timestamp_ms));
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    let dateString = `${months[date.getMonth()]} ${date.getDate()}`
+
     return (
       <Tweet
         key={tweetData.id_str}
@@ -19,6 +29,9 @@ const TwitterFeed = (props) => {
         favorited={tweetData.favorited}
         retweeted={tweetData.retweeted}
         mediaUrl={media_url}
+        displayUrl={display_url}
+        iconAlertProp={iconAlert}
+        dateStringProp={dateString}
       />
     );
   });
